b901c0fb8efa1ec08a555bc166269f44f93df91d
Cai-wei <s3444705@student.rmit.edu.au>
2015-09-19 13:26:11 +1000

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Contact')
 
-@section('page_title', 'Contact Us'))
+@section('page_title', 'Contact Us')
  
 @section('top_content')
     <p>Please leave your details and we will get back to you shortly</p>
