e3c182b3ab68ff3293b6840fabce8378234e8ec4
Cai-wei <s3444705@student.rmit.edu.au>
2015-09-19 13:25:10 +1000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
       
     </p> 
 
-    <p><a class="btn btn-primary btn-lg" href="/about" role="button">Learn more &raquo;</a></p>
+    <p><a class="btn btn-info btn-lg" href="/about" role="button">Learn more &raquo;</a></p>
 @endsection
 
 
@@ -22,21 +22,37 @@
 @section('bottom_content')
 
 <!-- Example row of columns -->
-      <div class="row">
+       <div class="row">
         <div class="col-md-4">
+        <div class="thumbnail">
+        <img src="/img/01.jpg" alt="Sample Image">
+        <div class="caption">
           <h2>House 1</h2>
           <p>Lets put description and image of house from database here!</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        </div>
         </div>
         <div class="col-md-4">
+        <div class="thumbnail">
+        <img src="/img/15.jpg" alt="Sample Image">
+        <div class="caption">
           <h2>House 2</h2>
           <p>Lets put description and image of house from database here!</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+       </div>
+       </div>
        </div>
         <div class="col-md-4">
-          <h2>Heading</h2>
+        <div class="thumbnail">
+        <img src="/img/22.jpg" alt="Sample Image">
+        <div class="caption">
+          <h2>House 3</h2>
           <p>Lets put description and image of house from database here!</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        </div>
+        </div>
         </div>
 
 @endsection
